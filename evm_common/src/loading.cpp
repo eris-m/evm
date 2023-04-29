f9917c7f9c90ccb2c6c855b88d4d72b4d621ab74
erism <redpanda1675@gmail.com>
2023-04-29 16:14:07 +1000

    Added string loading/saving

--- evm_common/src/loading.cpp
+++ evm_common/src/loading.cpp
@@ -1,14 +1,26 @@
 #define EVM_INTERNAL
 #include <evm/loading.h>
 
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <type_traits>
+
 namespace evm
 {
+
+template <typename T>
+static uint64_t
+basic_size ()
+{
+  return sizeof (T);
+}
 
 template <typename T, typename A>
 static uint64_t
 basic_size (const A)
 {
-  return sizeof (T);
+  return basic_size<T> ();
 }
 
 template <typename T>
@@ -35,16 +47,80 @@ basic_save (const T &value, uint8_t *buff)
     buff[i] = bytes[i];
 }
 
+static uint64_t
+string_load_size (const uint8_t *buffer)
+{
+  return basic_load<uint64_t> (buffer);
+}
+
+template <typename S>
+static uint64_t
+string_save_size (const S &s)
+{
+  return s.length ();
+}
+
+static std::string
+string_load (const uint8_t *buffer)
+{
+  auto size = string_load_size (buffer);
+  buffer += basic_size<uint64_t> ();
+
+  std::string str;
+  str.reserve (size);
+
+  for (uint64_t i = 0; i < size; i++)
+    str[i] = static_cast<char> (buffer[i]);
+
+  return str;
+}
+
+static std::string_view
+string_view_load (const uint8_t *)
+{
+  throw std::runtime_error (
+      "Tried to load std::string_view from memory (which is not supported).");
+}
+
+template <typename S>
+static void
+string_save (const S &s, uint8_t *buffer)
+{
+  for (uint64_t i = 0; i < string_save_size (s); i++)
+    buffer[i] = static_cast<uint8_t> (s[i]);
+}
+
 template <typename T>
 ls_info<T>
 primitive_ls_info ()
 {
-  return evm::ls_info<T>{
+  return ls_info<T>{
     .load_size = basic_size<T>,
     .save_size = basic_size<T>,
     .load = basic_load<T>,
     .save = basic_save<T>,
   };
+}
+
+template <>
+ls_info<std::string>
+primitive_ls_info<std::string> ()
+{
+  return ls_info<std::string>{ .load_size = string_load_size,
+                               .save_size = string_save_size<std::string>,
+                               .load = string_load,
+                               .save = string_save<std::string> };
+}
+
+template <>
+ls_info<std::string_view>
+primitive_ls_info<std::string_view> ()
+{
+  return ls_info<std::string_view>{ .load_size = string_load_size,
+                                    .save_size
+                                    = string_save_size<std::string_view>,
+                                    .load = string_view_load,
+                                    .save = string_save<std::string_view> };
 }
 
 #define PRIMITIVE_LS_INFO(T) template ls_info<T> primitive_ls_info<T> ()
