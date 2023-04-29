9a302c34dc355a8af641607cecc3dccf33fd6a57
erism <redpanda1675@gmail.com>
2023-04-29 15:26:32 +1000

    Merged basic_ls_size and basic_sv_size

--- evm_common/src/loading.cpp
+++ evm_common/src/loading.cpp
@@ -1,16 +1,9 @@
 #define EVM_INTERNAL
 #include <evm/loading.h>
 
-template <typename T>
+template <typename T, typename A>
 static uint64_t
-basic_ld_size (const uint8_t *)
-{
-  return sizeof (T);
-}
-
-template <typename T>
-static uint64_t
-basic_save_size (const T &)
+basic_size (const A)
 {
   return sizeof (T);
 }
@@ -44,12 +37,13 @@ evm::ls_info<T>
 evm::primitive_ls_info ()
 {
   return evm::ls_info<T>{
-    .load_size = basic_ld_size<T>,
-    .save_size = basic_save_size<T>,
+    .load_size = basic_size<T>,
+    .save_size = basic_size<T>,
     .load = basic_load<T>,
     .save = basic_save<T>,
   };
 }
+
 
 
 #define PRIMITIVE_LS_INFO(T) template evm::ls_info<T> evm::primitive_ls_info<T> ()
